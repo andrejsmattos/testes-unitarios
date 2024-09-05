4d74103e05abd24fc7dc6fd159c1cbd77cbdbebe
André <andrejusmattos@gmail.com>
2024-09-05 11:07:59 -0300

    verificação de ngModel

--- src/app/calculadora/calculadora.component.spec.ts
+++ src/app/calculadora/calculadora.component.spec.ts
@@ -30,4 +30,11 @@ describe('CalculadoraComponent', () => {
     expect(component.resultado).toBe(7);
   });
 
+  it('deve verificar se ngModel está atualizando variáveis corretamente', () => {	
+    component.num1 = 295;
+    component.num2 = 217;
+    expect(component.num1).toBe(295);
+    expect(component.num2).toBe(217);
+  });
+
 });
